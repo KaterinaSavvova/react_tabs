78a565db906f837f600e600617bfb81c97939153
KatrinaSavvova <kat.savvek@gmail.com>
2020-09-08 19:25:49 +0300

    Updates

---
+++ precache-manifest.b4a99cad24df484788a7092ff261e45d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40be7e4c097d173b55eb407327bbafff",
+    "url": "./index.html"
+  },
+  {
+    "revision": "dee4d745702d5b504023",
+    "url": "./static/css/main.48ac6f28.chunk.css"
+  },
+  {
+    "revision": "141d707c2238da1fe2fc",
+    "url": "./static/js/2.221e00be.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.221e00be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dee4d745702d5b504023",
+    "url": "./static/js/main.7a0df771.chunk.js"
+  },
+  {
+    "revision": "53d1114eedfc0eaca354",
+    "url": "./static/js/runtime-main.a6df9e19.js"
+  }
+]);
